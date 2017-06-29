bfd76b70f0adcdf03e56bbb650a40686fa406bd3
bisaga <igor.babic@bisaga.com>
2017-06-29 21:45:26 +0200

    change request mapping

--- src/main/java/com/bisaga/myapp/CurrencyController.java
+++ src/main/java/com/bisaga/myapp/CurrencyController.java
@@ -18,28 +18,23 @@ public class CurrencyController {
     @Autowired private CurrencyService service;
 
     @RequestMapping(method=RequestMethod.GET)
-    @ResponseStatus(HttpStatus.FOUND)
     public List<CurrencyDto> findAll() {
         return service.getAll();
     }
 
     @RequestMapping(value="/{code}", method=RequestMethod.GET)
-    @ResponseStatus(HttpStatus.FOUND)
     public CurrencyDto findByCode(@PathVariable("code") String code) {
         return service.getByCode(code);
     }
 
     @RequestMapping(method=RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
     public CurrencyDto saveCurrency(@RequestBody CurrencyDto currency) {
         return service.saveCurrency(currency);
     }
 
     @RequestMapping(method=RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.OK)
-    public Integer deleteCurrency(@RequestParam(value = "rowid", required = true) Integer rowId ) {
+    public Integer deleteCurrency(@RequestParam(value = "rowid") Integer rowId ) {
         return service.delete(rowId);
     }
-
 
 }
